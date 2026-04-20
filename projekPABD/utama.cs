d148a992a26376449792367f0c6b9fec9a4b6a2c
Yuixixixixi <yendra.paundra@gmail.com>
2026-04-20 08:10:43 +0700

    Add cell coloring and LoadLaporan for payment status
    Implemented dgv_CellFormatting to visually distinguish "Lunas" and "Belum Lunas" statuses in DataGridViews. Subscribed relevant DataGridViews to this handler. Added LoadLaporan method to fetch and pivot monthly payment status, defaulting missing values to "Belum Lunas".

--- projekPABD/utama.cs
+++ projekPABD/utama.cs
@@ -18,3 +18,43 @@ namespace projekPABD
             dgvLaporan.CellFormatting += dgv_CellFormatting;
             dgvLaporanFull.CellFormatting += dgv_CellFormatting;
         }
+        // Fungsi mewarnai tabel
+        private void dgv_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            if (e.Value != null)
+            {
+                if (e.Value.ToString() == "Lunas")
+                {
+                    e.CellStyle.BackColor = Color.LightGreen;
+                    e.CellStyle.ForeColor = Color.Black;
+                }
+                else if (e.Value.ToString() == "Belum Lunas")
+                {
+                    e.CellStyle.BackColor = Color.MistyRose;
+                    e.CellStyle.ForeColor = Color.Red;
+                }
+            }
+        }
+        private void LoadLaporan()
+        {
+            SqlConnection conn = konn.GetConn();
+            try
+            {
+                conn.Open();
+                string sql = @"SELECT id_usaha, nama_pemilik, nama_usaha, no_wa,
+                               ISNULL([1], 'Belum Lunas') AS Jan, ISNULL([2], 'Belum Lunas') AS Feb,
+                               ISNULL([3], 'Belum Lunas') AS Mar, ISNULL([4], 'Belum Lunas') AS Apr,
+                               ISNULL([5], 'Belum Lunas') AS Mei, ISNULL([6], 'Belum Lunas') AS Jun,
+                               ISNULL([7], 'Belum Lunas') AS Jul, ISNULL([8], 'Belum Lunas') AS Ags,
+                               ISNULL([9], 'Belum Lunas') AS Sep, ISNULL([10], 'Belum Lunas') AS Okt,
+                               ISNULL([11], 'Belum Lunas') AS Nov, ISNULL([12], 'Belum Lunas') AS Des
+                               FROM (
+                                   SELECT p.id_usaha, p.nama_pemilik, p.nama_usaha, p.no_wa, b.bulan, b.status_bayar
+                                   FROM pelaku_usaha p
+                                   LEFT JOIN pembayaran b ON p.id_usaha = b.id_usaha AND b.tahun = 2026
+                               ) AS SourceTable
+                               PIVOT (
+                                   MAX(status_bayar)
+                                   FOR bulan IN ([1],[2],[3],[4],[5],[6],[7],[8],[9],[10],[11],[12])
+                               ) AS PivotTable";
+
