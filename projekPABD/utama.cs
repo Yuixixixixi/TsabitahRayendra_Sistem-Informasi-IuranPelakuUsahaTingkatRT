6c9ea5a39eb158cbb668076abdbf47e032c50df2
Yuixixixixi <yendra.paundra@gmail.com>
2026-05-18 05:43:59 +0700

    membuat fungsi private untuk komponen laporan dinamis

--- projekPABD/utama.cs
+++ projekPABD/utama.cs
@@ -27,6 +27,27 @@ namespace projekPABD
             BuatKomponenLaporanDinamis();
         }
 
+        private void BuatKomponenLaporanDinamis()
+        {
+            Label lblTahun = new Label { Text = "Tahun Laporan :", Location = new Point(520, 395), Size = new Size(110, 23), Font = new Font("Arial", 9, FontStyle.Bold) };
+            tabLaporan.Controls.Add(lblTahun);
+
+            numTahunLaporan = new NumericUpDown { Location = new Point(635, 393), Size = new Size(70, 22), Minimum = 2020, Maximum = 2099, Value = tahunBerjalan };
+            numTahunLaporan.ValueChanged += NumTahunLaporan_ValueChanged;
+            tabLaporan.Controls.Add(numTahunLaporan);
+
+            Label lblTarif = new Label { Text = "Tarif Per@ (Rp) :", Location = new Point(520, 430), Size = new Size(110, 23), Font = new Font("Arial", 9, FontStyle.Bold) };
+            tabLaporan.Controls.Add(lblTarif);
+
+            numIuranLaporan = new NumericUpDown { Location = new Point(635, 428), Size = new Size(100, 22), Minimum = 0, Maximum = 1000000, Value = nominalIuran, Increment = 5000 };
+            numIuranLaporan.ValueChanged += NumIuranLaporan_ValueChanged;
+            tabLaporan.Controls.Add(numIuranLaporan);
+
+            btnUpdateTarif = new Button { Text = "Simpan Tarif Tahun Ini", Location = new Point(750, 390), Size = new Size(160, 60), BackColor = Color.LightSkyBlue, FlatStyle = FlatStyle.Flat, Font = new Font("Arial", 9, FontStyle.Bold) };
+            btnUpdateTarif.Click += btnUpdateTarif_Click;
+            tabLaporan.Controls.Add(btnUpdateTarif);
+        }
+
 
     }
 }
