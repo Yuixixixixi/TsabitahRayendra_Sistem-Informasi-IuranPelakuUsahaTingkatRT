3e19057698dd1801ef2f10962a51e56d7f65d165
Yuixixixixi <yendra.paundra@gmail.com>
2026-05-18 06:05:48 +0700

    update

--- projekPABD/Koneksi.cs
+++ projekPABD/Koneksi.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.SqlClient; // WAJIB ADA UNTUK SQL SERVER
+using System.Data.SqlClient; 
 
 namespace projekPABD
 {
@@ -12,7 +12,7 @@ namespace projekPABD
         public SqlConnection GetConn()
         {
             SqlConnection conn = new SqlConnection();
-            // Sesuaikan Data Source dengan nama Server SQL kamu
+            
             conn.ConnectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=DB_IuranRT;Integrated Security=True";
             return conn;
         }
