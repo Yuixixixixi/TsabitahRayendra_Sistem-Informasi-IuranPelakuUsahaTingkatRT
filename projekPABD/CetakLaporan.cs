f0fdb481ca9aea36613910df8b76e6a99c0d210c
Yuixixixixi <yendra.paundra@gmail.com>
2026-06-23 03:29:22 +0700

    Add CetakLaporan form class scaffold with dependencies

---
+++ projekPABD/CetakLaporan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using CrystalDecisions.CrystalReports.Engine;
+
+namespace projekPABD
+{
+    public partial class CetakLaporan : Form
+    {
+        private int _tahun;
+
+        
+    }
+}
